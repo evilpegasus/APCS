237c55a7e99d4c85c67868e3bbfb0ef978602b1b
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2019-12-11 07:48:51 -0800

    5.3

--- Chapter5/src/Labs/MiniQuiz.java
+++ Chapter5/src/Labs/MiniQuiz.java
@@ -13,38 +13,53 @@ public class MiniQuiz
    //-----------------------------------------------------------------
    public static void main (String[] args)
    {
-      Question q1, q2;
-      String possible;
+      Question q1 = new Question("", "");
+      Question q2 = new Question("", "");
+      askQuestion(q1, "What is the capital of Jamaica?", "Kingston", 4);
+      askQuestion(q1, "Which is worse, ignorance or apathy?", "I don't know and I don't care", 10);
+      // String possible;
 
-      Scanner scan = new Scanner(System.in);
+      // Scanner scan = new Scanner(System.in);
 
-      q1 = new Question ("What is the capital of Jamaica?",
-                         "Kingston");
-      q1.setComplexity (4);
+      // q1 = new Question ("What is the capital of Jamaica?",
+      //                    "Kingston");
+      // q1.setComplexity (4);
 
-      q2 = new Question ("Which is worse, ignorance or apathy?",
-                         "I don't know and I don't care");
-      q2.setComplexity (10);
+      // q2 = new Question ("Which is worse, ignorance or apathy?",
+      //                    "I don't know and I don't care");
+      // q2.setComplexity (10);
 
-      System.out.print (q1.getQuestion());
-      System.out.println (" (Level: " + q1.getComplexity() + ")");
-      possible = scan.nextLine();
-      if (q1.answerCorrect(possible))
-         System.out.println ("Correct");
-      else
-         System.out.println ("No, the answer is " + q1.getAnswer());
+      // System.out.print (q1.getQuestion());
+      // System.out.println (" (Level: " + q1.getComplexity() + ")");
+      // possible = scan.nextLine();
+      // if (q1.answerCorrect(possible))
+      //    System.out.println ("Correct");
+      // else
+      //    System.out.println ("No, the answer is " + q1.getAnswer());
 
-      System.out.println();
-      System.out.print (q2.getQuestion());
-      System.out.println (" (Level: " + q2.getComplexity() + ")");
-      possible = scan.nextLine();
-      if (q2.answerCorrect(possible))
-         System.out.println ("Correct");
-      else
-         System.out.println ("No, the answer is " + q2.getAnswer());
+      // System.out.println();
+      // System.out.print (q2.getQuestion());
+      // System.out.println (" (Level: " + q2.getComplexity() + ")");
+      // possible = scan.nextLine();
+      // if (q2.answerCorrect(possible))
+      //    System.out.println ("Correct");
+      // else
+      //    System.out.println ("No, the answer is " + q2.getAnswer());
    }
 
-   public void askQuestion(String question, String answer, int level) {
+   private static void askQuestion(Question q, String question, String answer, int level) {
+      String possible;
+      Scanner scan = new Scanner(System.in);
+      q = new Question (question, answer);
+      q.setComplexity (level);
+      System.out.print (q.getQuestion());
+      System.out.println (" (Level: " + q.getComplexity() + ")");
+      possible = scan.nextLine();
+      if (q.answerCorrect(possible))
+         System.out.println ("Correct");
+      else
+         System.out.println ("No, the answer is " + q.getAnswer());
 
+      System.out.println();
    }
 }
