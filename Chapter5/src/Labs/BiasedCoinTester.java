f0014333857e807da8918a9fd8bc8fe3d2209e8f
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2019-12-12 07:35:49 -0800

    5.5

--- Chapter5/src/Labs/BiasedCoinTester.java
+++ Chapter5/src/Labs/BiasedCoinTester.java
@@ -4,9 +4,30 @@ public class BiasedCoinTester {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         BiasedCoin x, y, z;
+        int xCount = 0;
+        int yCount = 0;
+        int zCount = 0;
+
+
         x = new BiasedCoin();
+        System.out.println("Enter first bias: ");
         y = new BiasedCoin(scan.nextDouble());
+        System.out.println("Enter second bias: ");
         z = new BiasedCoin(scan.nextDouble());
 
+        for (int i = 1; i <= 100; i++) {
+            if (x.flip()) {
+                xCount ++;
+            }
+            if (y.flip()) {
+                yCount ++;
+            }
+            if (z.flip()) {
+                zCount ++;
+            }
+        }
+        System.out.println("Bias x: " + xCount);
+        System.out.println("Bias y: " + yCount);
+        System.out.println("Bias z: " + zCount);
     }
 }
