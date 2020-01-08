a82befd87e098677d623dc722166b5bfc480d8f0
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2020-01-08 08:30:16 -0800

    6.1

--- Chapter6/Labs/arrayFun.java
+++ Chapter6/Labs/arrayFun.java
@@ -1,30 +1,51 @@
+
+import java.util.Arrays;
+
 public class arrayFun {
 
 
     public static void main(String args[]) {
         int a[] = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
         int b[] = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
-        int sum1 = 0;
-        int sum2 = 0;
 
-        for (int i = 3; i <= 6; i++) {
-            sum1 += a[i];
+        System.out.println(sum(a, 3, 6));
+        System.out.println(sum(a, 2, 9));
+
+        System.out.println(count(a, 4));
+        System.out.println(count(a, 9));
+        System.out.println(count(a, 7));
+
+        int removed[] = remove(a, 7);
+        System.out.println(Arrays.toString(removed));
+        System.out.println(count(removed, 7));
+    }
+
+    public static int sum(int[] a, int start, int stop) {
+        int sum = 0;
+        for (int i = start; i <= stop; i++) {
+            sum += a[i];
         }
-        System.out.println(sum1);
-
-        for (int i = 2; i <= 9; i++) {
-            sum2 += a[i];
+        return sum;
+    }
+    public static int count(int[] a, int b) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b) {
+                count ++;
+            }
         }
-        System.out.println(sum2);
+        return count;
     }
-
-    public int sum(int[] a, int start, int stop) {
-        return 0;
-    }
-    public int count(int[] a, int b) {
-        return 0;
-    }
-    public int[] remove(int[] a, int b) {
-        return null;
+    public static int[] remove(int[] a, int b) {
+        int x[] = new int[a.length - count(a, b)];
+        int c = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b) {
+                x[i - c] = a[i];
+            } else {
+                 c++;
+            }
+        }
+        return x;
     }
 }
---
+++ Chapter6/Labs/arrayOdd.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class arrayOdd {
+
+    public static void main(String args[]) {
+        int [] Array1 = {2,4,6,8,10,12,14};
+        int [] Array2 = {2,3,4,5,6,7,8,9};
+        int [] Array3 = {2,10,20,21,23,24,40,55,60,61};
+
+    }
+
+    public static void odds() {
+        
+    } 
+}
