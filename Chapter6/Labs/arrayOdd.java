362f4611282d3b0d92831826cd7cc692f44e81df
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2020-01-09 08:30:49 -0800

    6.1

--- Chapter6/Labs/arrayOdd.java
+++ Chapter6/Labs/arrayOdd.java
@@ -7,9 +7,34 @@ public class arrayOdd {
         int [] Array2 = {2,3,4,5,6,7,8,9};
         int [] Array3 = {2,10,20,21,23,24,40,55,60,61};
 
+        odds(Array1);
+        odds(Array2);
+        odds(Array3);
     }
 
-    public static void odds() {
-        
-    } 
+    public static void odds(int a[]) {
+        int oddCount = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                oddCount++;
+            }
+        }
+        int evenCount = a.length - oddCount;
+
+        int odd[] = new int[oddCount];
+        int even[] = new int[evenCount];
+        int o = 0;
+        int e = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                odd[o] = a[i];
+                o++;
+            } else {
+                even[e] = a[i];
+                e++;
+            }
+        }
+        System.out.println("Odd: " + Arrays.toString(odd));
+        System.out.println("Even: " + Arrays.toString(even));
+    }
 }
