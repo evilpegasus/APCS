32b4bcfa66a9a280027ee264b152a79590192aa6
Ming Fong <mingfong07@hotmail.com>
2020-01-30 22:14:51 -0800

    6.11 add files

---
+++ Chapter6/Labs/ArrayListFunHouse.java
@@ -0,0 +1,21 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class ArrayListFunHouse
+{
+	public static ArrayList<Integer> getListOfFactors(int number)
+	{
+		return null;
+	}
+	
+	public static void keepOnlyCompositeNumbers( List<Integer> nums )
+	{
+	}
+}
