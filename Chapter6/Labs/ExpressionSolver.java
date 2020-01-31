ee1d2345ad6896a4a1abed43e49c1027642bb0da
Ming Fong <mingfong07@hotmail.com>
2020-01-30 23:23:32 -0800

    6.12 in progress

--- Chapter6/Labs/ExpressionSolver.java
+++ Chapter6/Labs/ExpressionSolver.java
@@ -4,27 +4,46 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.*;
 import static java.lang.Integer.*;
 import static java.lang.System.*;
 
 public class ExpressionSolver
 {
 	//add in instance variables
+	String expression;
+	Integer answer;
 
 	public ExpressionSolver(String s)
 	{
+		expression = s;
 	}
 
 	public void setExpression(String s)
 	{
+		expression = s;
 	}
 
 	public void solveExpression()
 	{
+		//Convert to ArrayList
+		ArrayList<String> a = new ArrayList<String>();
+		for (int i = 0; i < expression.length(); i++) {
+			if (expression.charAt(i) != ' ') {
+				a.add(String.valueOf(expression.charAt(i)));
+			}
+		}
+		System.out.println(Arrays.toString(a.toArray()));
+
+		//compute * and /
+		if (a.indexOf("*") > 0 || a.indexOf("/") > 0) {
+			
+		}
 	}
 
 	public String toString( )
 	{
-		return "";
+		return null;
+		//return (expression + " = " + Integer.toString(answer));
 	}
 }
