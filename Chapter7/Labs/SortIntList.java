e4a5cb8979f9a4b98831baf52fc88cdcb6f2dda3
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2020-02-06 08:30:14 -0800

    7.2 ip

---
+++ Chapter7/Labs/SortIntList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SortIntList extends IntList{
+    public SortIntList(int size) {
+        super(size);
+    }
+
+    public void add(int x) {
+        
+    }
+}
