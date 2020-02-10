f1e6c0b5ec6d2038dfa3169f27f22eb43db5171b
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2020-02-10 08:23:13 -0800

    7.2 asdf

--- Chapter7/Labs/SortIntList.java
+++ Chapter7/Labs/SortIntList.java
@@ -6,6 +6,15 @@ public class SortIntList extends IntList{
     }
 
     public void add(int x) {
+        if (numElements == list.length)
+	    System.out.println("Can't add, list is full");
+	    else {
+		    for (int i = 0; i < list.length; i++) {
+                if (x < list[i]) {
+                    
+                }
+            }
+	    }
         
     }
 }
