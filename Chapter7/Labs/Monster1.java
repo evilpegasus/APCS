70d7f95b9721f4ac7c744465a7c793c052aa9df8
Ming Fong <mingfong07@hotmail.com>
2020-02-27 23:52:04 -0800

    7.9 upload new files

---
+++ Chapter7/Labs/Monster1.java
@@ -0,0 +1,65 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class Monster1 implements Comparable
+{
+	private int myWeight;
+	private int myHeight;
+	private int myAge;
+
+	//write a default Constructor
+
+
+
+	//write an initialization constructor with an int parameter ht
+
+
+
+	//write an initialization constructor with int parameters ht and wt
+
+
+
+
+	//write an initialization constructor with int parameters ht, wt, and age
+
+
+
+	//modifiers - write set methods for height, weight, and age
+
+	
+	
+	//accessors - write get methods for height, weight, and age
+	
+	
+	
+	//creates a new copy of this Object
+	public Object clone()
+	{
+	   return new Monster();
+	}
+
+	public boolean equals( Object obj )
+	{
+
+
+		return false;
+	}
+
+	public int compareTo( Object obj )
+	{
+		Monster rhs = (Monster)obj;
+
+
+
+
+
+		return -1;
+	}
+
+	//write a toString() method
+	
+	
+}
