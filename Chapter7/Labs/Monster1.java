55a276c25f20afbafc7497b48a73406adbdf7690
Ming Fong <mingfong07@hotmail.com>
2020-03-02 21:44:07 -0800

    7.9 Done

--- Chapter7/Labs/Monster1.java
+++ Chapter7/Labs/Monster1.java
@@ -1,3 +1,5 @@
+package Chapter7.Labs;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -11,55 +13,94 @@ public class Monster1 implements Comparable
 	private int myAge;
 
 	//write a default Constructor
-
+	public Monster1() {
+		myWeight = 0;
+		myHeight = 0;
+		myAge = 0;
+	}
 
 
 	//write an initialization constructor with an int parameter ht
-
+	public Monster1(int ht) {
+		myWeight = 0;
+		myHeight = ht;
+		myAge = 0;
+	}
 
 
 	//write an initialization constructor with int parameters ht and wt
-
+	public Monster1(int ht, int wt) {
+		myWeight = wt;
+		myHeight = ht;
+		myAge = 0;
+	}
 
 
 
 	//write an initialization constructor with int parameters ht, wt, and age
-
+	public Monster1(int ht, int wt, int age) {
+		myWeight = wt;
+		myHeight = ht;
+		myAge = age;
+	}
 
 
 	//modifiers - write set methods for height, weight, and age
-
+	public void setHeight(int ht) {
+		myHeight = ht;
+	}
+	public void setWeight(int wt) {
+		myWeight= wt;
+	}
+	public void setAge(int age) {
+		myAge = age;
+	}
 	
 	
 	//accessors - write get methods for height, weight, and age
-	
+	public int getHeight() {
+		return myHeight;
+	}
+	public int getWeight() {
+		return myWeight;
+	}
+	public int getAge() {
+		return myAge;
+	}
 	
 	
 	//creates a new copy of this Object
 	public Object clone()
 	{
-	   return new Monster();
+	   return new Monster1(myHeight, myWeight, myAge);
 	}
 
 	public boolean equals( Object obj )
 	{
-
-
-		return false;
+		if (myHeight == ((Monster1)obj).getHeight() && myWeight == ((Monster1)obj).getWeight() && myAge == ((Monster1)obj).getAge()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public int compareTo( Object obj )
 	{
-		Monster rhs = (Monster)obj;
-
-
-
-
-
-		return -1;
+		Monster1 rhs = (Monster1)obj;
+		if (myHeight != rhs.getHeight()) {
+			return ((Integer)myHeight).compareTo(rhs.getHeight());
+		} else if (myWeight != ((Monster1)rhs).getWeight()) {
+			return ((Integer)myWeight).compareTo(rhs.getWeight());
+		} else if (myAge != rhs.getAge()) {
+			return ((Integer)myAge).compareTo(rhs.getAge());
+		} else {
+			return 0;
+		}
 	}
 
 	//write a toString() method
-	
+	public String toString() {
+		return ("Height: " + myHeight + "\tWeight: " + myWeight + "\tAge: " + myAge);
+	}
 	
 }
---
+++ Chapter7/Labs/MonsterRunner.java
@@ -0,0 +1,50 @@
+package Chapter7.Labs;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class MonsterRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		//ask for name and size
+		System.out.println("Size 1?");
+		int size1 = scan.nextInt();
+		System.out.println("Name 1?");
+		String name1 = scan.nextLine();	//There's a line break issue that's skipping the input for name but the rest works fine
+		//instantiate monster one
+		Skeleton s1 = new Skeleton(size1, name1);
+		//ask for name and size
+		System.out.println("Size 2?");
+		int size2 = scan.nextInt();
+		System.out.println("Name 2?");
+		String name2 = scan.nextLine();
+		//instantiate monster two
+		Skeleton s2 = new Skeleton(size2, name2);
+
+		System.out.println(s1);
+		System.out.println(s2);
+
+		String compareSize;
+		if (s1.isBigger(s2)) {
+			compareSize = "larger than ";
+		} else if (!s1.isBigger(s2)) {
+			compareSize = "smaller than ";
+		} else {
+			compareSize = "the same size as ";
+		}
+		System.out.println("Monster 1 is " + compareSize + "monster 2.");
+
+		if (s1.namesTheSame(s2)) {
+			System.out.println("They have the same names.");
+		} else {
+			System.out.println("They do not have the same names.");
+		}
+	}
+}
